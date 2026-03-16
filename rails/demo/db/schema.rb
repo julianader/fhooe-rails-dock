bc0701d6599451f66650ce6c7efd73a4a12ca90b
julianader <julianader4@gmail.com>
2026-03-16 10:03:00 +0100

    Create User Model

--- rails/demo/db/schema.rb
+++ rails/demo/db/schema.rb
@@ -10,8 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_16_090017) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_090206) do
   create_table "entries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
