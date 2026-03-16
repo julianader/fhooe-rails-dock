8736d263af86adb038d17f212d816cc4d0ad545a
julianader <julianader4@gmail.com>
2026-03-16 11:11:44 +0100

    Adding Categories to Articles in lab2

--- rails/lab2/db/schema.rb
+++ rails/lab2/db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_16_093259) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_101007) do
   create_table "articles", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
